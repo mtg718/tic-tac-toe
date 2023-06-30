bfd4e8b3c4c4ad7e568867f83a7cd81e03781952
Mohit Gupta <135738292+mtg718@users.noreply.github.com>
2023-06-30 12:08:57 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -90,7 +90,7 @@ return false;
 
     }
 
-    // printboard funvtion
+    // printboard function
     public static void printBoard(char board[][]) {
         for (int row = 0; row < board.length; row++) {
             for (int col = 0; col < board[row].length; col++) {
